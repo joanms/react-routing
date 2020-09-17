39fd358a189086a9b612fd3c3f30cd1000247410
Joan Sweeney <joanms@eircom.net>
2020-09-17 17:02:03 +0100

    Redirect from / to /posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -33,7 +33,8 @@ class Blog extends Component {
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
                     <Route path="/new-post" component={NewPost} />
-                    <Route path="/posts/" component={Posts} />
+                    <Route path="/posts" component={Posts} />
+                    <Route path="/" component={Posts} />
                 </Switch>
             </div>
         );
